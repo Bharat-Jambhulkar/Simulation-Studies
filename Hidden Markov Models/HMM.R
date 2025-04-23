9c3519e5652536d508dd671ee7ca78fb7479c56a
Bharat-Jambhulkar <145669348+Bharat-Jambhulkar@users.noreply.github.com>
2025-04-23 09:12:45 +0530

    Update HMM
    HMM Generated. Figure Out Estimation Part Using Viterbi Algorithm

--- Hidden Markov Models/HMM.R
+++ Hidden Markov Models/HMM.R
@@ -1,16 +1,61 @@
-### Generate a Markov Chain ####
+## Generate a observed and an unobserved series 
 
-Y = c()
-Y[1] = sample(c(1,2,3),prob=c(0.5,0.3,0.2),size=1)
-tpm = matrix(c(0.7,0.2,0.1,0.3,0.5,0.2,0.2,0.5,0.3),nrow=3,ncol=3,byrow = T)
+TPM = round(matrix(c(2/3,1/3,1/2,1/2),nrow=2,ncol=2,byrow = T),4)
 
-n=100
-st = c(1,2,3)
-for(i in 1:n){
-  if(Y[i]==1) Y[i+1] = sample(st,size=1,prob = c(0.7,0.2,0.1))
-  if(Y[i]==2) Y[i+1] = sample(st,size=1,prob = c(0.3,0.5,0.2))
-  if(Y[i]==3) Y[i+1] = sample(st,size=1,prob = c(0.2,0.5,0.3))
+## State space for Yt = {0,1}
+## Possible value for Xt = {2,3,4}
+## Write an emission matrix 
+
+E = round(matrix(c(1/3,2/3,2/3,1/3),nrow=2,ncol=2,byrow=T),4)
+
+Y_state = c(1,2)
+Yt = c()
+n=10
+Yt[1] = sample(Y_state,size = 1,prob=c(1/2,1/2))
+for(i in 2:n){
+  if(Yt[i-1] == 1) Yt[i] = sample(Y_state,size = 1,prob = TPM[1,])
+  if(Yt[i-1] == 2) Yt[i] = sample(Y_state,size = 1,prob = TPM[2,])
 }
 
-Y
+## Generate a observed chain 
+Xt = c()
+X_values = c(3,4)
+for(i in 1:n){
+  if(Yt[i] == 1) Xt[i] = sample(X_values,size=1,prob = c(1/3,2/3))
+  if(Yt[i] == 2) Xt[i] = sample(X_values,size=1,prob = c(2/3,1/3))
+}
+Xt
 
+pihatmat = matrix(nrow=1000,ncol=2)
+qmathat = matrix(nrow=2,ncol=2)
+tpmhat = matrix(nrow=2,ncol=2)
+
+qmathat[1,] = c(0.1,0.9)
+qmathat[2,] = c(0.6,0.4)
+
+pihatmat[1,] = c(0.5,0.5)
+
+tpmhat[1,1] = 0.5;tpmhat[2,1] = 0.5;tpmhat[1,2] = 0.5;tpmhat[2,2] = 0.5
+
+V = matrix(nrow=1000,ncol=2)
+
+yhat = c()
+#yhat[1] = sample(c(1,2),size=1,prob=c(0.5,0.5))
+Xt[1]
+
+V[1,1] = pihatmat[1,1]*qmathat[1,2]
+
+V[1,2] = pihatmat[1,2]*qmathat[2,2]
+head(V)
+for(i in 2:length(Xt)){
+  if(Xt[i]==3){
+    V[i,1] = V[i-1,1]*tpmhat[1,1]*qmathat[1,1]
+    V[i,2] = V[i-1,2]*tpmhat[2,1]*qmathat[2,1]
+  } 
+  if(Xt[i]==4){
+    V[i,1] = V[i-1,1]*tpmhat[1,2]*qmathat[1,2]
+    V[i,2] = V[i-1,2]*tpmhat[2,2]*qmathat[2,2]
+  }
+  yhat[i-1] = which.max(V[i-1,])
+}
+yhat
